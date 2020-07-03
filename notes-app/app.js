5402a960526e30e546ebfe0b04d7f2f42b9015b6
Dari Alves <darieltondf@gmail.com>
2020-07-02 22:23:49 -0300

    first commit

---
+++ notes-app/app.js
@@ -0,0 +1,5 @@
+const fs = require('fs');
+const os = require('os');
+const _ = require('lodash');
+const notes = require('./notes');
+
---
+++ notes-app/notes.js
@@ -0,0 +1,6 @@
+console.log('hello notes');
+
+module.exports.addNote = () => {
+    return 'New note'
+}
+
